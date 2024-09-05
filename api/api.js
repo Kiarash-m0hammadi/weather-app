c9af440f6f90d2035646e028806f1f74f408e6c5
Kiarash-m0hammadi <kiarash7.mohammadi@gmail.com>
2024-09-05 16:35:52 +0330

    Refactor store.js to use single quotes for imports and remove unnecessary semicolons

--- api/api.js
+++ api/api.js
@@ -1,91 +1,91 @@
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 
 //https://app.ipgeolocation.io/dashboard
 
 async function getCities(query) {
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
-      throw new Error(`Error fetching city data: ${response.status}`);
+      throw new Error(`Error fetching city data: ${response.status}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error("Error fetching city data:", error);
-    return null;
+    console.error('Error fetching city data:', error)
+    return null
   }
 }
 
 async function getWeatherData(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
-      throw new Error(`Error fetching weather data: ${response.status}`);
+      throw new Error(`Error fetching weather data: ${response.status}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error("Error fetching weather data:", error);
-    return null;
+    console.error('Error fetching weather data:', error)
+    return null
   }
 }
 
 async function getForecast(city) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
-      throw new Error(`Error fetching forecast data: ${response.status}`);
+      throw new Error(`Error fetching forecast data: ${response.status}`)
     }
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
 
     function groupByDate(data) {
-      const groups = {};
+      const groups = {}
       data.list.forEach((item) => {
-        const dateString = new Date(item.dt_txt).toLocaleDateString();
+        const dateString = new Date(item.dt_txt).toLocaleDateString()
         if (!groups[dateString]) {
-          groups[dateString] = [];
+          groups[dateString] = []
         }
-        groups[dateString].push(item);
-      });
-      return groups;
+        groups[dateString].push(item)
+      })
+      return groups
     }
 
     // Example usage
-    const groupedByDate = groupByDate(data);
+    const groupedByDate = groupByDate(data)
 
-    console.log(groupedByDate);
+    console.log(groupedByDate)
 
-    return data;
+    return data
   } catch (error) {
-    console.error("Error fetching forecast data:", error);
-    return null;
+    console.error('Error fetching forecast data:', error)
+    return null
   }
 }
 
 async function getAQIForecast(lon, lat) {
-  const url = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
       throw new Error(
-        `Error fetching air quality forecast data: ${response.status}`
-      );
+        `Error fetching air quality forecast data: ${response.status}`,
+      )
     }
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const data = await response.json()
+    console.log(data)
+    return data
   } catch (error) {
-    console.error("Error fetching air quality forecast data:", error);
-    return null;
+    console.error('Error fetching air quality forecast data:', error)
+    return null
   }
 }
 
-export { getCities, getWeatherData, getForecast, getAQIForecast };
+export { getCities, getWeatherData, getForecast, getAQIForecast }
